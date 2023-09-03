3e1f8d816638c96d7190ca3fef4395147b1425a2
Iamneo <techadmin@iamneo.ai>
2023-09-03 15:32:09 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,25 +1,40 @@
-
+// Define constants for player symbols
 const PLAYER_X = 'X';
 const PLAYER_O = 'O';
+
+// Initialize game variables
 let currentPlayer = PLAYER_X;
 let isGameActive = true;
+
+// Get all the buttons (grid cells)
 const buttons = document.querySelectorAll('.btn');
+
+// Get the result container and reset button
 const resultContainer = document.querySelector('.result');
 const resetButton = document.getElementById('reset-btn');
+
+// Function to handle button click
 function handleButtonClick(event) {
     const button = event.target;
+
+    // Check if the button is empty and the game is active
     if (button.textContent === '' && isGameActive) {
         button.textContent = currentPlayer;
-        button.classList.add('filled');
+        button.classList.add('filled', currentPlayer.toLowerCase());
+
+        // Check for a win or draw
         if (checkWin() || checkDraw()) {
             isGameActive = false;
             resultContainer.textContent = isGameActive ? `Player ${currentPlayer}'s Turn` : 'Game Over';
         } else {
+            // Toggle players
             currentPlayer = currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
             resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
         }
     }
 }
+
+// Function to check for a win
 function checkWin() {
     const winPatterns = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
@@ -38,6 +53,7 @@ function checkWin() {
     return false;
 }
 
+// Function to check for a draw
 function checkDraw() {
     const isDraw = [...buttons].every(button => button.textContent !== '');
     if (isDraw) {
@@ -46,17 +62,23 @@ function checkDraw() {
     }
     return false;
 }
+
+// Function to reset the game
 function resetGame() {
     buttons.forEach(button => {
         button.textContent = '';
-        button.classList.remove('filled');
+        button.classList.remove('filled', 'x', 'o');
     });
 
     currentPlayer = PLAYER_X;
     isGameActive = true;
     resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
 }
+
+// Add click event listeners to the buttons
 buttons.forEach(button => {
     button.addEventListener('click', handleButtonClick);
 });
+
+// Add click event listener to the reset button
 resetButton.addEventListener('click', resetGame);
